b5a9e208ae39dfd78a327947182f3aa6e116a097
Maria Letícia Dantas <leticiacadantas@gmail.com>
2023-07-07 16:55:23 -0300

    Resolve exercícios

---
+++ fundamentos/secao-3/dia-2-arrays/shopping.js
@@ -0,0 +1,13 @@
+const shopping = ['Sabão em pó', 'Macarrão', 'Leite'];
+
+for (let index = 0; index < shopping.length; index += 1) {
+    console.log(`${index + 1} - ${shopping[index]}`);
+}
+
+const cars = ['Saab', 'Volvo', 'BMW', 'Chevrolet'];
+
+for (let index = 0; index < cars.length; index += 1) {
+    const currentCar = cars[index];
+    console.log(currentCar);
+  };
+  
